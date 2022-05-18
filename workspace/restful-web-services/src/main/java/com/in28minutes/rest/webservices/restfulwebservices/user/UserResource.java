0e73f1f0e847dbbe5998f60b02975d916a722dae
billy7175 <whgus439@gmail.com>
2022-05-19 03:44:42 +0900

    post /users

--- workspace/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ workspace/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,4 +24,11 @@ public class UserResource {
 	public User retriveUser (@PathVariable int id) {
 		return service.findOne(id);
 	}
+	
+	@PostMapping("/users")
+	public User createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		return savedUser;
+	}
+	
 }
